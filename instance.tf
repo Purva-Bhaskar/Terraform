9f6a71eaeec2e8925703d68806e82eedc87b159e
purvabhaskar <bhaskarpurva2415@gmail.com>
2026-02-26 19:06:57 +0530

    cm3

--- instance.tf
+++ instance.tf
@@ -6,6 +6,6 @@ resource "aws_instance" "first" {
     ami = "ami-0f3caa1cf4417e51b"
     instance_type = "t2.micro"
     tags = {
-        Name = "EC2-terra"
+        Name = "EC2-terraform"
     }
 }
