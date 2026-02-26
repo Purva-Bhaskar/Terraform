3fcdb26d1dfe7c9a74e6be2452adcf372bc2788e
purvabhaskar <bhaskarpurva2415@gmail.com>
2026-02-26 19:04:10 +0530

    cm2

---
+++ instance.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "first" {
+    ami = "ami-0f3caa1cf4417e51b"
+    instance_type = "t2.micro"
+    tags = {
+        Name = "EC2-terra"
+    }
+}
