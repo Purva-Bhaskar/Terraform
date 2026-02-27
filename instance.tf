252be3f39bf3ee6e3c374e4ced4bb9cafdcf80e4
purvabhaskar <bhaskarpurva2415@gmail.com>
2026-02-27 10:25:59 +0530

    instance file

---
+++ instance.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_instance" "fct-instance-1" {
+    for_each = toset(["jump server","Application server","db instance"])
+    instance_type = "t3.micro"
+    ami = "ami-0f3caa1cf4417e51b"
+    tags = {
+      Name = 
+    }
+}
