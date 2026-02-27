758d52a1e4ebf3ae820ad64ec49f5e7f460dc48c
purvabhaskar <bhaskarpurva2415@gmail.com>
2026-02-27 10:47:27 +0530

    instance changed

--- instance.tf
+++ instance.tf
@@ -2,11 +2,20 @@ provider "aws" {
     region = "us-east-1"
 }
 
+# resource "aws_instance" "fct-instance-1" {
+#     for_each = toset(["jump server","Application server","db instance"])
+#     instance_type = "t3.micro"
+#     ami = "ami-0f3caa1cf4417e51b"
+#     tags = {
+#       Name = each.key
+#     }
+# }
+
 resource "aws_instance" "fct-instance-1" {
-    for_each = toset(["jump server","Application server","db instance"])
-    instance_type = "t3.micro"
-    ami = "ami-0f3caa1cf4417e51b"
-    tags = {
-      Name = 
-    }
+        instance_type = "t3.micro"
+        ami = "ami-0f3caa1cf4417e51b"
+        count = 3
+        tags = {
+        Name = "web-server${count.index+1}"
+        }
 }
